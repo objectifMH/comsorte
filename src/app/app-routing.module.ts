7a4f4fc3aa7330e2e4131966ad618ea7ed78c736
unknown <objectifmh@hotmail.com>
2021-04-18 11:36:51 +0200

    routerlink schroll

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "main", component: AppComponent},
+  // {path: '**', redirectTo: '/main' },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
