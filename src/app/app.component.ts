8579acdc549dbfa64797a70082c0c7321117d966
unknown <objectifmh@hotmail.com>
2021-04-18 10:15:44 +0200

    mobile first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Comsorte';
+
+  langueForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+
+  ngOnInit() {
+    this.langueForm = this.fb.group({
+      countryControl: ['FR']
+    });
+  }
 }
