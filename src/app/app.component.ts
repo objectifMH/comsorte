ca09081503f1bf934385fa88b145ec6cade22394
unknown <objectifmh@hotmail.com>
2021-04-22 17:59:25 +0200

    test langue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,50 @@ export class AppComponent {
   title = 'Comsorte';
 
   langueForm: FormGroup;
+  isShowDrapeauxLangues = false;
+  langue = "fr";
+  srcLangue = "assets/icon/france.svg"
+  langues = [
+    {
+      "pays":"fr",
+      "src": "france.svg"
+    },
+    {
+      "pays":"es",
+      "src": "paraguay.svg"
+    },
+    {
+      "pays":"en",
+      "src": "england.svg"
+    },
+    {
+      "pays":"br",
+      "src": "brazil.svg"
+    }
+  ];
+
+
+
 
   constructor(private fb: FormBuilder) { }
 
 
   ngOnInit() {
-    
     AOS.init();
     this.langueForm = this.fb.group({
       countryControl: ['FR']
     });
+  }
+
+  showDrapeauxLangues(lang) {
+    this.langue = lang;
+    this.langues.forEach(lg => {
+      if (lg.pays === lang) {
+        this.srcLangue = "assets/icon/" + lg.src;
+      }
+    })
+    this.isShowDrapeauxLangues = !this.isShowDrapeauxLangues;
+    
   }
 
 }
