62347696664aa3986bef1742b24b0d26cbbe3cf1
unknown <objectifmh@hotmail.com>
2021-05-04 11:20:08 +0200

    apparition au scroll en js et css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,22 +14,23 @@ export class AppComponent {
   langueForm: FormGroup;
   isShowDrapeauxLangues = false;
   langue = "fr";
+
   srcLangue = "assets/icon/france.svg"
   langues = [
     {
-      "pays":"fr",
+      "pays": "fr",
       "src": "france.svg"
     },
     {
-      "pays":"es",
+      "pays": "es",
       "src": "paraguay.svg"
     },
     {
-      "pays":"en",
+      "pays": "en",
       "src": "england.svg"
     },
     {
-      "pays":"br",
+      "pays": "br",
       "src": "brazil.svg"
     }
   ];
@@ -37,15 +38,70 @@ export class AppComponent {
 
 
 
+
   constructor(private fb: FormBuilder, private translate: TranslateService) {
     translate.setDefaultLang('fr');
-   }
+  }
 
 
   ngOnInit() {
+    // Animation 
     AOS.init();
+
+    // Langue internationalisation 
     this.langueForm = this.fb.group({
       countryControl: ['FR']
+    });
+
+    // Animation au scroll 
+
+
+    document.addEventListener('scroll', function (e) {
+      let top = window.pageYOffset + window.innerHeight;
+
+
+      // Apparition au scroll aqui items : 
+
+      let aqui_conteneur = document.getElementById('id_aqui');
+      let anais_conteneur = document.getElementById('id_anais');
+
+
+      let top_aqui = aqui_conteneur.offsetTop;
+      let top_anais = anais_conteneur.offsetTop;
+
+      let aqui_items = document.querySelectorAll('.aqui_item');
+
+      (top > top_aqui + 200 && top < top_anais) ?
+        aqui_items.forEach(items => {
+          items.classList.add('glissement_animation');
+        })
+        :
+        aqui_items.forEach(items => {
+          items.classList.remove('glissement_animation');
+        })
+
+
+
+      // Apparition au Scroll Chiffres items
+
+      let chiffre_conteneur = document.getElementById('id_chiffres');
+      let formule_conteneur = document.getElementById('id_formules');
+
+
+      let top_chiffre = chiffre_conteneur.offsetTop;
+      let top_formule = formule_conteneur.offsetTop;
+
+      let chiffres_items = document.querySelectorAll('.chiffre_item');
+
+
+      (top > top_chiffre + 200 && top < top_formule + 300) ?
+        chiffres_items.forEach(items => {
+          items.classList.add('glissement_animation');
+        })
+        :
+        chiffres_items.forEach(items => {
+          items.classList.remove('glissement_animation');
+        })
     });
   }
 
@@ -60,9 +116,9 @@ export class AppComponent {
       }
     })
     this.isShowDrapeauxLangues = !this.isShowDrapeauxLangues;
-    
+
   }
 
-  
+
 
 }
